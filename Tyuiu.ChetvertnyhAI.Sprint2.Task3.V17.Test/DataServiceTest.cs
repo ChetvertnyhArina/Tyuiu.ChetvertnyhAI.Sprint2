679fd2992134eadc1c524c119fbdc6926d6c18f9
ChetvertnyhArina <chetvertnyh-arina@mail.ru>
2025-10-15 23:01:51 +0500

    Task3

---
+++ Tyuiu.ChetvertnyhAI.Sprint2.Task3.V17.Test/DataServiceTest.cs
@@ -0,0 +1,51 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint2.Task3.V17.Lib;
+namespace Tyuiu.ChetvertnyhAI.Sprint2.Task3.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 3;
+            double res = ds.Calculate(x);
+            double wait = 265;
+            Assert.AreEqual(wait, res);
+
+        }
+
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = -0.286;
+            Assert.AreEqual(wait, res);
+        }
+
+
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = 16;
+            Assert.AreEqual(wait, res);
+        }
+
+
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -22;
+            double res = ds.Calculate(x);
+            double wait = -241.955;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ChetvertnyhAI.Sprint2.Task3.V17/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint2.Task3.V17.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #2 | Выполнила: Четвертных А. И. | ИСТНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #17                                                             *");
+        Console.WriteLine("* Выполнила: Четвертных Арина Игоревна | ИСТНб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y c    *");
+        Console.WriteLine("* использованием вложенных оператор if-else, где пользователь вводит      *");
+        Console.WriteLine("* значение переменной X с клавиатуры. Округлить полученное значение       *");
+        Console.WriteLine("* до трех знаков после запятой.                                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите значение переменной X: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        double res = ds.Calculate(x);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Значение функции = " + res);
+
+        Console.ReadKey();
+    }
+}
