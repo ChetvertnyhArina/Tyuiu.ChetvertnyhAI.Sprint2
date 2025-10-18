34b66c0dceedeeb4fc6225926321bf4d2b38cba2
ChetvertnyhArina <chetvertnyh-arina@mail.ru>
2025-10-18 10:40:18 +0500

    Task7

--- Tyuiu.ChetvertnyhAI.Sprint2.Task7.V4.Lib/DataService.cs
+++ Tyuiu.ChetvertnyhAI.Sprint2.Task7.V4.Lib/DataService.cs
@@ -6,15 +6,29 @@ namespace Tyuiu.ChetvertnyhAI.Sprint2.Task7.V4.Lib
         public bool CheckDotInShadedArea(double x, double y)
         {
             bool res;
-            if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) && (((x >= -1) && (x < 0) && ((y > 0) && (x >= -1))) || ((x >= 0.3) && (x <= 1) && ((y >= 0.3) && (x <= 1)))))
+            if (((Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) && (((x >= -1) && (x < 0) && ((y > 0) && (x >= -1))) || ((x > 0) && (x <= 1) && ((y > 0) && (x <= 1))))))
             {
                 res = true;
+            }
+            else if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 0.09) && ((x > 0) && (x < 0.3) && (y > 0) && (y < 0.3)))
+            {
+                res = false;
             }
             else
             {
                 res = false;
             }
+
+            //&& (((x >= -1) && (x <= 0) && (y >= 0) && (y <= 1)) || ((x >= 0.3) && (x <= 1) && (y >= 0.3) && (y <= 1)))
+            //if (Math.Pow(x, 2) + Math.Pow(y, 2) <= 1)
+            //{
+            //    if (y > 0)
+            //    {
+            //        if ((Math.Pow(x, 2) - Math.Pow(y, 2)))
+            //    }
+            //}
             return res;
-        }      
+        }    
+              
     }
 }
